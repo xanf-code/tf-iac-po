e7dc5b6e3ea56f1085ca06201ed642203b9ab72e
xanf-code <darshanaswath@gmail.com>
2025-02-22 21:14:10 -0500

    changed run time environment

--- secrets.tf
+++ secrets.tf
@@ -1,8 +1,6 @@
 resource "aws_secretsmanager_secret" "po_gateway_resource_id_secret" {
-  name = "${var.all_vars_prefix}-secrets-api-url-external"
-  lifecycle {
-    prevent_destroy = true
-  }
+  name = "${var.all_vars_prefix}-secrets-api-url-external-v1"
+  recovery_window_in_days = 0
 }
 
 resource "aws_secretsmanager_secret_version" "po_gateway_resource_id_secret_version" {
